609a12264be1dc111c1e4cc86dfb475390b8a242
NL <nliampisan@gmail.com>
2015-07-23 18:10:22 +0700

    written all functions

--- myscript.js
+++ myscript.js
@@ -4,6 +4,12 @@ var inventory = new Array();
 //create temp purchase 
 var purchase_list = new Array(); 
 
+//create favorite list 
+var fav_list = new Array(); 
+for (var x = 0 ; x <= 10;  x++) 
+	fav_list[x] = null; 
+
+
 // total cost of transaction 
 var one_total = 0; 
 
@@ -113,17 +119,124 @@ var erase = function() {
 		var item_tol = purchase_list[i].quan*(purchase_list[i].price);  
 
 		//update the amount of items left in stock
-		alert(inventory[a].quan); 
-		alert( purchase_list[i].quan); 
-		inventory[a].quan += purchase_list[i].quan; 
+		/*alert(inventory[a].quan); 
+		alert( purchase_list[i].quan); */
+		inventory[a].quan = parseInt(inventory[a].quan)+parseInt(purchase_list[i].quan); 
 
 		//updating total cost
 		one_total -= item_tol; 
 		document.getElementById('screen').innerHTML = one_total;	
 
+		
+		
 
+		// updating scanned items
+		var node = document.createElement("LI");
+   	 	var textnode = document.createTextNode("--"+purchase_list[i].name +"@"+purchase_list[i].price+"*"+purchase_list[i].quan+"="+"-"+item_tol);
+    		node.appendChild(textnode);
+    		document.getElementById("alist").appendChild(node);
+
+
+		//remove item from list 
+		purchase_list[i].name = null; 
 	}
+
 }
+
+var checkout = function() {
+	// do change calculation 
+	var money = prompt("Money given:");
+
+	if (money < one_total) 
+	{
+		alert("insufficient money"); 
+	}
+	else
+	{ 
+	var change = money-one_total; 
+	alert("Change: " + change); 
+
+
+	//add total to sales and reset one_total
+	sales += parseFloat(one_total); 
+	one_total = 0; 
+	// updating one_total back to zero 
+	document.getElementById('screen').innerHTML = one_total;
+
+	// delete everything in list 
+	document.getElementById("alist").innerHTML = "";
+
+
+	// problem with reseting array 
+	// reset user array 
+	purchase_list = new Array(); 	
+	}
+	
+}
+
+
+var checksales = function() {
+	alert("Total sales: " + sales); 
+
+}
+
+var addfav = function() {
+	var name = prompt("What is the item's name?"); 
+	var i = search(name); 
+	if (i === -1) 
+	{
+		alert("Item not found"); 
+	}
+	else
+	{ 
+		var index = prompt("Which favorite button number? 1-10"); 
+		if (index<= 10 && index >=1) 
+		{
+
+			//updating button 
+			document.getElementById(index).innerHTML = name;	
+			// add item to purchase list 
+			fav_list[index] = inventory[i].name ; 
+
+		}
+		else
+		{
+			alert("invalid index"); 
+		}
+	}
+	
+}
+
+var sellfav = function (a) {
+		if (fav_list[a] !== null) 
+		{			
+			var amount = prompt("Quantity that you're selling?");
+			var i = search(fav_list[a]); 
+			var item_tol = amount*(inventory[i].price); 
+			
+			//update the amount of items left in stock
+		inventory[i].quan -= amount;
+
+			// updating scanned items
+			var node = document.createElement("LI");
+   	 		var textnode = document.createTextNode(inventory[i].name +"@"+inventory[i].price+"*"+amount+"="+item_tol);
+    			node.appendChild(textnode);
+    			document.getElementById("alist").appendChild(node);
+			//updating total cost
+		one_total += item_tol; 
+		document.getElementById('screen').innerHTML = one_total;	
+
+
+		// add item to purchase list 
+		purchase_list[purchase_list.length] = new item(inventory[i].name, inventory[i].price, amount); 	
+		}
+		else
+		{
+			alert("has not been added to favorites"); 
+		}
+
+}
+
 
 var print_all = function() {
 	for (var i = 0 ; i < inventory.length; i++) 
